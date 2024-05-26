721087ed2e05f712201890e9b5bf9c74fb9f7381
dilyar111 <153483424+dilyar111@users.noreply.github.com>
2024-05-26 20:02:48 +0500

    commit

---
+++ src/Vertex.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+
+public class Vertex<T> {
+    private T data;
+    private Map<Vertex<T>, Double> adjacentVertices;
+
+    // getters&setters constructors
+    // equals
+}
+
